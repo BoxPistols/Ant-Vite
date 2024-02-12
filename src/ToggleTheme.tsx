a6ef07722b2f0173c3699f9afe424f716d78b050
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-02-12 18:24:27 +0900

    ToggleTheme Basic

---
+++ src/ToggleTheme.tsx
@@ -0,0 +1,68 @@
+/**
+https://ant.design/docs/react/customize-theme
+ */
+
+import {
+  ConfigProvider,
+  Space,
+  Button,
+  theme,
+  Row,
+  Typography,
+  Col,
+  Flex,
+  
+} from "antd";
+import { useState } from "react";
+
+const ToggleTheme: React.FC = () => {
+  const [changeMode, setChangeMode] = useState(false);
+
+  const switchTheme = () => {
+    setChangeMode(!changeMode);
+  };
+
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: changeMode ? theme.compactAlgorithm : theme.defaultAlgorithm,
+        token: {
+          // Seed Token
+          colorPrimary: "#00b96b",
+          borderRadius: 2,
+          // Alias Token
+          colorBgContainer: "#f6ffed",
+        },
+      }}
+    >
+      <Typography.Title level={1}>Ant Design</Typography.Title>
+      <Flex vertical gap={24}>
+        <Row gutter={16}>
+          <Col>
+            <Space>
+              <Button
+                onClick={switchTheme}
+                type={!changeMode ? "dashed" : "primary"}
+              >
+                {!changeMode ? "to compactAlgorithm" : "to defaultAlgorithm"}
+              </Button>
+            </Space>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col>
+            <Space>
+              <Button>Default Button</Button>
+              <Button type="primary">Button</Button>
+              <Button type="dashed">Dashed Button</Button>
+              <Button type="link">Link Button</Button>
+            </Space>
+          </Col>
+        </Row>
+      </Flex>
+    </ConfigProvider>
+  );
+};
+
+export default ToggleTheme;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import  App  from "./App.tsx";
+import ToggleTheme from "./ToggleTheme.tsx";
 import "./index.css";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <ToggleTheme />
   </React.StrictMode>
 );
