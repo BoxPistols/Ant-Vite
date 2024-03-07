2665188b51e08c241fad7552a37ac64df3566606
a.ito <BoxPistols@users.noreply.github.com>
2024-03-08 08:36:23 +0900

    PR TEST (#21)
    Co-authored-by: a.ito <10333049+BoxPistols@users.noreply.github.com>

--- src/Layout/Content.tsx
+++ src/Layout/Content.tsx
@@ -10,7 +10,7 @@ const Content: React.FC<ContentProps> = ({ children, text = 'Hello' }) => {
     <>
       <Typography.Title level={2}>Content Comp</Typography.Title>
       <Typography>{text}</Typography>
-      {text === 'Hello' && <Typography>Default text is used</Typography>}
+      {text === 'Hello' && <Typography>Default text</Typography>}
       {children}
     </>
   )
